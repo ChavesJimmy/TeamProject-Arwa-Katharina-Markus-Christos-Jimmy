b79eab60a187bad2f4602b21c73d0ad48ae38607
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-10-17 13:34:34 +0200

    gallery page added

---
+++ photographerWebpage/src/app/galleryContent/galleryContent.ts
@@ -0,0 +1,77 @@
+export const mainImage : Array<any> = [
+    {
+        image :"https://cdn.pixabay.com/photo/2017/08/06/20/11/wedding-2595862__480.jpg",
+        title:"Wedding"},
+    {
+        image :"https://cdn.pixabay.com/photo/2017/05/13/12/40/fashion-2309519__480.jpg",
+        title:"Portrait"},
+    {
+        image :"https://cdn.pixabay.com/photo/2017/08/08/03/50/family-2610205__480.jpg",
+        title:"Family"},
+    {
+        image :"https://cdn.pixabay.com/photo/2016/11/22/19/15/audience-1850119__480.jpg",
+        title:"Party"},
+    {
+        image :"https://cdn.pixabay.com/photo/2017/12/13/11/57/birthday-3016615__480.jpg",
+        title:"Birthday"},
+    {
+        image :"https://cdn.pixabay.com/photo/2015/06/19/09/39/lonely-814631__480.jpg",
+        title:"Model"},
+    {   image :"https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297__480.jpg",
+        title:"Natur"},
+]
+export const myGalleryWedding : Array<any> = [
+    {
+        url : "",
+        photos : [""]
+        
+    }
+];
+
+export const myGalleryPortrait : Array<any> = [
+    {
+        url : "",
+        photos : [""]
+
+    }
+];
+
+export const myGalleryFamily : Array<any> = [
+    {
+        url : "",
+        photos : [""]
+
+    }
+];
+
+export const myGalleryParty : Array<any> = [
+    {
+        url : "",
+        photos : [""]
+
+    }
+];
+
+export const myGalleryBirthday : Array<any> = [
+    {
+        url : "",
+        photos : [""]
+
+    }
+];
+
+export const myGalleryModel : Array<any> = [
+    {
+        url : "",
+        photos : [""]
+
+    }
+];
+
+export const myGalleryNatur : Array<any> = [
+    {
+        url : "",
+        photos : [""]
+
+    }
+];
