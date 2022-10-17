3109b4363679a4c139873ca63c48d5598d62c122
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-10-17 13:38:25 +0200

    added route to gallery

--- photographerWebpage/src/app/app-routing.module.ts
+++ photographerWebpage/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 const routes: Routes = [
   {
     path: "about",
     component: AboutComponent,
   },
+  {
+    path:"gallery", component:GalleryComponent
+  }
 ];
 
 @NgModule({
