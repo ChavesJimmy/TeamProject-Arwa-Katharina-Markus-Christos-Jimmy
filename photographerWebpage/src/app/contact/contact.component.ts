1836614b5fbcaa0b1b3a8444bafc6eb390d92dba
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-10-18 14:49:46 +0200

    added basic contact form

--- photographerWebpage/src/app/contact/contact.component.ts
+++ photographerWebpage/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  Contact = new FormGroup ({
+    firstName: new FormControl('',Validators.required),
+    email: new FormControl('',Validators.required),
+    date: new FormControl('',Validators.required),
+    lastName: new FormControl('',Validators.required),
+    age: new FormControl('',Validators.required),
+    message: new FormControl('',Validators.required),
+
+  })
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+
+    if(this.Contact.valid){
+
+      var a = this.Contact.value;
+      alert(``)
+      console.log(a)
+   }
+   else{
+    alert(`one or more infos are missing`)
+   }
+
+  }
 }
