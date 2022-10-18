688ad61c5f40a45e32e4356341cf3cc7744ba7af
Chrisyur <chris-yurt@hotmail.com>
2022-10-18 11:06:30 +0200

    adds dynamic testimonial generation

---
+++ photographerWebpage/src/app/testimonials/testimonials-scripts/iTestimonialsList.ts
@@ -0,0 +1,9 @@
+export interface iTestimonialsList {
+  firstName: string;
+  lastName: string;
+  position?: string;
+  email: string;
+  company?: string;
+  website?: string;
+  description: string;
+}
--- photographerWebpage/src/app/testimonials/testimonials.component.ts
+++ photographerWebpage/src/app/testimonials/testimonials.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { iTestimonialsList } from './testimonials-scripts/iTestimonialsList';
+import { testimonialsList } from './testimonials-scripts/testimonialsList';
 
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.css']
+  styleUrls: ['./testimonials.component.css'],
 })
 export class TestimonialsComponent implements OnInit {
+  testimonialsList: iTestimonialsList[] = testimonialsList;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
