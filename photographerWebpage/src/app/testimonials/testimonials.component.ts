526e18f310488af4105a8d88ce4953d341f87a17
Chrisyur <chris-yurt@hotmail.com>
2022-10-18 16:39:22 +0200

    adds testimonial form functionality (no style yet)

--- photographerWebpage/src/app/testimonials/testimonials.component.ts
+++ photographerWebpage/src/app/testimonials/testimonials.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { iTestimonialsList } from './testimonials-scripts/iTestimonialsList';
 import { testimonialsList } from './testimonials-scripts/testimonialsList';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-testimonials',
@@ -9,8 +11,39 @@ import { testimonialsList } from './testimonials-scripts/testimonialsList';
 })
 export class TestimonialsComponent implements OnInit {
   testimonialsList: iTestimonialsList[] = testimonialsList;
+  testimonial: iTestimonialsList = {} as iTestimonialsList;
 
-  constructor() {}
+  testimonialForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    position: '',
+    company: '',
+    description: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    if (this.testimonialForm.value.name != null) {
+      this.testimonial.name = this.testimonialForm.value.name;
+    }
+    if (this.testimonialForm.value.email != null) {
+      this.testimonial.email = this.testimonialForm.value.email;
+    }
+    if (this.testimonialForm.value.position != null) {
+      this.testimonial.position = this.testimonialForm.value.position;
+    }
+    if (this.testimonialForm.value.company != null) {
+      this.testimonial.company = this.testimonialForm.value.company;
+    }
+    if (this.testimonialForm.value.description != null) {
+      this.testimonial.description = this.testimonialForm.value.description;
+    }
+
+    this.testimonialsList.push(this.testimonial);
+    this.testimonial = {} as iTestimonialsList;
+    this.testimonialForm.reset();
+  }
 
   ngOnInit(): void {}
 }
