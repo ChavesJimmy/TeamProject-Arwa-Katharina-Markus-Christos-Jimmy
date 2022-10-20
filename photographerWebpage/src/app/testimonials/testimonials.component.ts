6ba18c9759f69c89a2e8d71e598fb2095ffa4f76
Chrisyur <chris-yurt@hotmail.com>
2022-10-20 10:44:06 +0200

    adds sweet alert for testimonial form

--- photographerWebpage/src/app/testimonials/testimonials.component.ts
+++ photographerWebpage/src/app/testimonials/testimonials.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { iTestimonialsList } from './testimonials-scripts/iTestimonialsList';
 import { testimonialsList } from './testimonials-scripts/testimonialsList';
-import { FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-testimonials',
@@ -24,25 +24,49 @@ export class TestimonialsComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   onSubmit() {
-    if (this.testimonialForm.value.name != null) {
-      this.testimonial.name = this.testimonialForm.value.name;
-    }
-    if (this.testimonialForm.value.email != null) {
-      this.testimonial.email = this.testimonialForm.value.email;
-    }
-    if (this.testimonialForm.value.position != null) {
-      this.testimonial.position = this.testimonialForm.value.position;
-    }
-    if (this.testimonialForm.value.company != null) {
-      this.testimonial.company = this.testimonialForm.value.company;
-    }
-    if (this.testimonialForm.value.description != null) {
-      this.testimonial.description = this.testimonialForm.value.description;
-    }
+    if (this.testimonialForm.valid) {
+      if (this.testimonialForm.value.name != null) {
+        this.testimonial.name = this.testimonialForm.value.name;
+      }
+      if (this.testimonialForm.value.email != null) {
+        this.testimonial.email = this.testimonialForm.value.email;
+      }
+      if (this.testimonialForm.value.position != null) {
+        this.testimonial.position = this.testimonialForm.value.position;
+      }
+      if (this.testimonialForm.value.company != null) {
+        this.testimonial.company = this.testimonialForm.value.company;
+      }
+      if (this.testimonialForm.value.description != null) {
+        this.testimonial.description = this.testimonialForm.value.description;
+      }
 
-    this.testimonialsList.push(this.testimonial);
-    this.testimonial = {} as iTestimonialsList;
-    this.testimonialForm.reset();
+      this.testimonialsList.push(this.testimonial);
+      this.testimonial = {} as iTestimonialsList;
+      this.testimonialForm.reset();
+
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        background: `rgb(33,37,41)`,
+        color: `white`,
+        title:
+          'Your testimonial was sent, we are thankful for sharing your opinion',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        background: `rgb(33,37,41)`,
+        color: `white`,
+        title: 'One or more mandatory fields are empty',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      this.testimonialForm.reset();
+    }
   }
 
   ngOnInit(): void {}
